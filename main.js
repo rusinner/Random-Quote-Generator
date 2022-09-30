0111a99299f9e45466f751a16b5ab7b04ca0bbf2
Theologos Mavropoulos <rusinner88@gmail.com>
2022-09-30 13:45:27 +0300

    api

--- main.js
+++ main.js
@@ -4,4 +4,16 @@ let quote = document.getElementById('quote');
 let author = document.getElementById('author');
 let btn = document.getElementById('btn');
 
-const url = '';
+//api url
+const url = 'https://api.quotable.io/random';
+
+
+//fetch data from api library
+let getQuote = () =>{
+    fetch(url).then(data => data.json())    
+    .then((item) => {
+        console.log(item);
+    });
+}
+
+window.addEventListener('load',getQuote);
